3cc0c4f8e796c95e3af40c1ea1ab6375c529f156
Benjamin Eberlei <kontakt@beberlei.de>
2014-07-18 19:11:44 +0200

    Simplify encoded uuid generation.

--- src/main/QafooLabs/Profiler.php
+++ src/main/QafooLabs/Profiler.php
@@ -567,20 +567,16 @@ class Profiler
             $uuid = base64_encode(
                 pack(
                     "h*",
-                    str_replace(
-                        '-',
-                        '',
-                        sprintf(
-                            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-                            mt_rand(0, 0xffff),
-                            mt_rand(0, 0xffff),
-                            mt_rand(0, 0xffff),
-                            mt_rand(0, 0x0fff) | 0x4000,
-                            mt_rand(0, 0x3fff) | 0x8000,
-                            mt_rand(0, 0xffff),
-                            mt_rand(0, 0xffff),
-                            mt_rand(0, 0xffff)
-                        )
+                    sprintf(
+                        '%04x%04x%04x%04x%04x%04x%04x%04x',
+                        mt_rand(0, 0xffff),
+                        mt_rand(0, 0xffff),
+                        mt_rand(0, 0xffff),
+                        mt_rand(0, 0x0fff) | 0x4000,
+                        mt_rand(0, 0x3fff) | 0x8000,
+                        mt_rand(0, 0xffff),
+                        mt_rand(0, 0xffff),
+                        mt_rand(0, 0xffff)
                     )
                 )
             );
